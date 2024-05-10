816a4f6f8e7e931e1d833232baa84d43a594c9fb
KirillS <gratesco60@gmail.com>
2024-05-10 23:21:33 +0300

    Добавил Решение Задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] FilterArrayByLength(string[] array, int maxLength)
+{
+    int count = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <= maxLength)
+        {
+            count++;
+        }
+    }
+
+    string[] result = new string[count];
+    int i = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <= maxLength)
+        {
+            result[i] = $"\"{str}\"";
+            i++;
+        }
+    }
+
+    return result;
+}
+
+
+Console.WriteLine("Введите строки для массива через запятую:");
+string[] initialArray = Console.ReadLine().Split(',');
+
+string[] newArray = FilterArrayByLength(initialArray, 3);
+
+Console.WriteLine("Новый массив");
+foreach (string str in newArray)
+{
+    Console.Write(string.Join(", ", str + " "));
+}
